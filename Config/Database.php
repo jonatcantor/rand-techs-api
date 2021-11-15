20eddb85732b7238cf0425d5f2c69170067d8c7c
Jonathan Cantor <jonatcantor@gmail.com>
2021-11-15 00:23:16 -0400

    Add port number in database env variables

--- Config/Database.php
+++ Config/Database.php
@@ -2,6 +2,7 @@
 class Database {
     private $host;
     private $db_name;
+    private $db_port;
     private $username;
     private $password;
     private $connection;
@@ -9,13 +10,14 @@ class Database {
     public function __construct() {
         $this->host = $_ENV['APP_HOST'];
         $this->db_name = $_ENV['APP_DB_NAME'];
+        $this->db_port = $_ENV['APP_DB_PORT'];
         $this->username = $_ENV['APP_DB_USERNAME'];
         $this->password = $_ENV['APP_DB_PASSWORD'];
     }
 
     public function connect() {
         try {
-            $this->connection = new PDO("mysql:host=$this->host; dbname=$this->db_name; charset=utf8", $this->username, $this->password);
+            $this->connection = new PDO("mysql:host=$this->host; port=$this->db_port; dbname=$this->db_name; charset=utf8", $this->username, $this->password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
