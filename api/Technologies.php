23bc40028ef376b4d19b7e89b2b1cb13425f1cfd
Jonathan Cantor <jonatcantor@gmail.com>
2021-11-16 06:33:32 -0400

    Detected database environment

--- api/Technologies.php
+++ api/Technologies.php
@@ -1,8 +1,8 @@
 <?php
 if(!isset($_ENV['APP_PRODUCTION'])) {
-    require_once(__DIR__ . '/../vendor/autoload.php');
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-    $dotenv->load();
+  require_once(__DIR__ . '/../vendor/autoload.php');
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+  $dotenv->load();
 }
 
 require_once(__DIR__ . '/../Config/Database.php');
@@ -18,58 +18,57 @@ $dbConnection = $database->connect();
 $technology = new Technology($dbConnection);
 
 if(!isset($_GET['command']) || $_GET['command'] == '') {
-    echo json_encode(['error' => 'error: command not found']);
-    return;
+  echo json_encode(['error' => 'error: command not found']);
+  return;
 }
 
 $command = htmlspecialchars($_GET['command']);
 $commandSplit = explode(' ', $command);
 
 if(count($commandSplit) != 2) {
-    echo json_encode(['error' => 'error: command not found']);
-    return;
+  echo json_encode(['error' => 'error: command not found']);
+  return;
 }
 
 if($commandSplit[0] == 'rand') {
-    define ('COMMANDS', [
-        'fron' => 'Frontend',
-        'back' => 'Backend',
-        'full' => 'FullStack'
-    ]);
+  define ('COMMANDS', [
+    'fron' => 'Frontend',
+    'back' => 'Backend',
+    'full' => 'FullStack'
+  ]);
 
-    if(!array_key_exists($commandSplit[1], COMMANDS)) {
-        echo json_encode(['error' => 'error: command not found']);
-    }
+  if(!array_key_exists($commandSplit[1], COMMANDS)) {
+    echo json_encode(['error' => 'error: command not found']);
+  }
 
-    else if($commandSplit[1] == 'full') {
-        $techElements = $technology->getFullRandom();
-        echo json_encode(['fron' => $techElements[0]->fetchAll(), 'back' => $techElements[1]->fetchAll()]);
-    }
+  else if($commandSplit[1] == 'full') {
+    $techElements = $technology->getFullRandom();
+    echo json_encode(['fron' => $techElements[0]->fetchAll(), 'back' => $techElements[1]->fetchAll()]);
+  }
 
+  else {
+    $techElements = $technology->getRandom(COMMANDS[$commandSplit[1]]);
+    echo json_encode([$commandSplit[1] => $techElements->fetchAll()]);
+  }
 
-    else {
-        $techElements = $technology->getRandom(COMMANDS[$commandSplit[1]]);
-        echo json_encode([$commandSplit[1] => $techElements->fetchAll()]);
-    }
-
-    return;
+  return;
 }
 
 if($commandSplit[0] == 'ecos') {
-    define ('COMMANDS', [
-        'react' => 'React'
-    ]);
+  define ('COMMANDS', [
+    'react' => 'React'
+  ]);
 
-    if(!array_key_exists($commandSplit[1], COMMANDS)) {
-        echo json_encode(['error' => 'error: command not found']);
-    }
+  if(!array_key_exists($commandSplit[1], COMMANDS)) {
+    echo json_encode(['error' => 'error: command not found']);
+  }
 
-    else {
-        $techElements = $technology->getEcosystem($commandSplit[1]);
-        echo json_encode(['ecos' => $techElements->fetchAll()]);
-    }
+  else {
+    $techElements = $technology->getEcosystem($commandSplit[1]);
+    echo json_encode(['ecos' => $techElements->fetchAll()]);
+  }
 
-    return;
+  return;
 }
 
 echo json_encode(['error' => 'error: command not found']);
