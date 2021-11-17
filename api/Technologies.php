b2565d81595fa3ffa9c617c81fd78df698719c52
Jonathan Cantor <jonatcantor@gmail.com>
2021-11-17 11:57:25 -0400

    Router controller from index and routes for each endpoint

--- api/Technologies.php
+++ api/Technologies.php
@@ -1,19 +1,5 @@
 <?php
-if(!isset($_ENV['APP_PRODUCTION'])) {
-  require_once(__DIR__ . '/../vendor/autoload.php');
-  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-  $dotenv->load();
-}
-
-require_once(__DIR__ . '/../Config/Database.php');
 require_once(__DIR__ . '/../Models/Technology.php');
-
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: ' . $_ENV['APP_ORIGIN']);
-header('Access-Control-Allow-Methods: GET');
-
-$database = new Database();
-$dbConnection = $database->connect();
 
 $technology = new Technology($dbConnection);
 
